0f91cb4f985ecd4610d6c75c39491584abb04c7f
KarelStevens <karel_stevens@hotmail.com>
2015-04-29 16:56:33 +0200

    imd questions

---
+++ Les 5/imdquestions/app.js
@@ -0,0 +1,18 @@
+// app.js
+
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+    socket.on('sendmessage', function(data) {
+        io.sockets.emit('newmessage', data);
+    });
+});
